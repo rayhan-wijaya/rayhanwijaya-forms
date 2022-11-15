ac548fda2b91b0cf6780adf2f3c35fedda73e406
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-15 23:21:11 +0700

    Created trpcClient.ts for initializing a tRPC client

---
+++ src/utils/trpcClient.ts
@@ -0,0 +1,32 @@
+import { httpBatchLink } from '@trpc/client';
+import { createTRPCNext } from '@trpc/next';
+import type { AppRouter } from 'server/routers/app.router';
+
+function getBaseUrl() {
+	if (typeof window !== 'undefined') {
+		return '';
+	}
+
+	if (process.env.VERCEL_URL) {
+		return `https://${process.env.VERCEL_URL}`;
+	}
+
+	if (process.env.RENDER_INTERNAL_HOSTNAME) {
+		return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+	}
+
+	return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export const trpcClient = createTRPCNext<AppRouter>({
+	config({ ctx }) {
+		return {
+			links: [
+				httpBatchLink({
+					url: `${getBaseUrl()}/api/trpc`,
+				}),
+			],
+		};
+	},
+	ssr: true,
+});
