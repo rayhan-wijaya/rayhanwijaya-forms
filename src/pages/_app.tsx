204487c7e0d6a9dfd9e29ab4a64e4602272c93a3
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-16 19:35:37 +0700

    Added withTRPC to NextJS _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppType } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
-}
+import { trpcClient } from '../utils/trpcClient'; 
+
+const App: AppType = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />;
+};
+
+export default trpcClient.withTRPC(App);
