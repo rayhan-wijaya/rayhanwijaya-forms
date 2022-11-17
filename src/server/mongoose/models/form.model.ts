c12d4ed0d132453f2aba3d289fe7cd994a9dc640
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-17 22:37:49 +0700

    Removed mongoose files

--- src/server/mongoose/models/form.model.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import formSchema from 'server/mongoose/schemas/form.schema';
-
-const formModel = mongoose.model('Form', formSchema);
-
-export default formModel;
--- src/server/mongoose/models/response.model.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import responseSchema from 'server/mongoose/schemas/response.schema';
-
-const responseModel = mongoose.model('Response', responseSchema);
-
-export default responseModel;
